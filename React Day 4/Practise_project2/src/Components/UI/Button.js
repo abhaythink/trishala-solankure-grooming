06aa6b2deb438e6a09091647ef1cb8ed4a3681bc
Trishala <trishala.solankure@thinkitive.com>
2025-06-05 19:11:03 +0530

    React Day 4

---
+++ React Day 4/Practise_project2/src/Components/UI/Button.js
@@ -0,0 +1,15 @@
+import classes from "./Button.module.css";
+
+let Button = (props) => {
+  return (
+    <button
+      className={classes.button}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
